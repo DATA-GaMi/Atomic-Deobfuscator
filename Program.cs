8f0066c4d587cdf1c5ec3062153979272856e5ff
Desec <happy123yuan00@aliyun.com>
2022-06-26 20:01:45 +0800

    net35,net48,net6.0

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace AtomicDeobfuscator
+{
+    class Program
+    {
+        private static void F(string filename)
+        {
+            Console.Title = "Atomic Deobfuscator";
+            Console.WriteLine("Atomic Deobfuscator");
+            ModuleDefMD module = ModuleDefMD.Load(filename);
+            Console.WriteLine("Hide methods: " + hideMethodsRemover.Deobfuscate(module));
+            ModuleWriterOptions mod = new ModuleWriterOptions(module);
+            mod.MetadataLogger = DummyLogger.NoThrowInstance;
+            string patho = Path.GetDirectoryName(filename) + "\\" + Path.GetFileNameWithoutExtension(filename) + "-Deobfuscated" + Path.GetExtension(filename);
+            module.Write(patho, mod);
+            Console.WriteLine("saved to " + patho);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"[Path]:");
+                Console.ResetColor();
+                string filename = Console.ReadLine().Replace(@"""", string.Empty);
+                F(filename);
+            }
+        }
+    }
+}
